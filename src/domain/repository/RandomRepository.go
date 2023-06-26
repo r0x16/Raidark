3f8ac4224d300310564adeed486c1eedd7024742
r0x16 <sir.thomas.lara@gmail.com>
2023-06-26 16:13:55 -0400

    feat(core): Add random functionalities examples
    - Add store random number in db
    - Add get all randoms from db

--- src/domain/repository/RandomRepository.go
+++ src/domain/repository/RandomRepository.go
@@ -4,4 +4,6 @@ import "git.tnschile.com/sistemas/tnsgo/raidark/src/domain/model"
 
 type RandomRepository interface {
 	GenerateRandomString(int) *model.Random
+	Store(*model.Random) error
+	GetAll() ([]*model.Random, error)
 }
--- src/shared/infraestructure/repository/RandomRepository.go
+++ src/shared/infraestructure/repository/RandomRepository.go
@@ -24,16 +24,35 @@ func NewRandomRepositoryGorm(db *gorm.DB) *RandomRepositoryBasic {
 func (*RandomRepositoryBasic) GenerateRandomString(n int) *model.Random {
 	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
-    rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 
-    b := make([]byte, n)
-    for i := range b {
-        b[i] = letters[rand.Intn(len(letters))]
-    }
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
 
 	random := &model.Random{
 		Value: string(b),
 	}
 
-    return random
+	return random
+}
+
+// GetAll implements repository.RandomRepository.
+func (r *RandomRepositoryBasic) GetAll() ([]*model.Random, error) {
+	var randoms []*model.Random
+	result := r.db.Find(&randoms)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return randoms, nil
+}
+
+// Store implements repository.RandomRepository.
+func (r *RandomRepositoryBasic) Store(random *model.Random) error {
+	result := r.db.Create(random)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
 }
